c862365deea81a79472fa9c8e2dbc44aa2246b70
Kevin Cottrill <KCottrill088@GMail.com>
2021-12-02 17:23:01 -0600

    Solutions from first day

---
+++ DepthTests.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace Depth;
+
+public class DepthTests
+{
+    [Fact]
+    public void TestIncreases()
+    {
+        var input = ReadInput(@"/home/kevin/repos/Depth/input");
+        var increases = Increases(input);
+        Assert.Equal(1451, increases);
+    }
+
+    [Fact]
+    public void TestWindowIncreases()
+    {
+        var input = ReadInput(@"/home/kevin/repos/Depth/input");
+        var increases = WindowIncreases(input); // new[] { 4, 4, 4, 5});
+        Assert.Equal(1395, increases);
+    }
+
+    private static int WindowIncreases(IList<int> depths)
+    {
+        if (depths.Count < 4) return 0;
+
+        var increases = 0;
+        for (var i = 3; i < depths.Count; i++)
+            if (depths[i] > depths[i - 3])
+                increases++;
+
+        return increases;
+    }
+
+    private static int Increases(IList<int> depths)
+    {
+        if (depths.Count < 2) return 0;
+
+        var increases = 0;
+        for (var i = 1; i < depths.Count; i++)
+        {
+            if (depths[i] > depths[i - 1])
+                increases++;
+        }
+
+        return increases;
+    }
+
+    private static IList<int> ReadInput(string fileName)
+    {
+        var input = new List<int>();
+        foreach (var line in System.IO.File.ReadLines(fileName))
+        {
+            if (int.TryParse(line, out var n))
+                input.Add(n);
+        }
+        return input;
+    }
+}
+
